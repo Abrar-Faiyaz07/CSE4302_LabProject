53a493d6b65928cb628d9fd2cec91a07935368af
Abrar-Faiyaz07 <abrarsamin04@gmail.com>
2026-02-17 00:21:24 +0600

    Added Book, Rating, User headers

---
+++ book-recommendation/include/Book.h
@@ -0,0 +1,42 @@
+﻿#ifndef BOOK_H
+#define BOOK_H
+
+#include <string>
+
+class Book {
+public:
+    Book();
+    Book(int id, const std::string& title, const std::string& author,
+         const std::string& genre, int year, double avgRating,
+         int ratingCount, int popularity);
+
+    int getId() const;
+    const std::string& getTitle() const;
+    const std::string& getAuthor() const;
+    const std::string& getGenre() const;
+    int getYear() const;
+    double getAvgRating() const;
+    int getRatingCount() const;
+    int getPopularity() const;
+
+    void setTitle(const std::string& title);
+    void setAuthor(const std::string& author);
+    void setGenre(const std::string& genre);
+    void setYear(int year);
+    void setPopularity(int popularity);
+
+    bool isValid() const;
+    void applyNewRating(int score);
+
+private:
+    int id;
+    std::string title;
+    std::string author;
+    std::string genre;
+    int year;
+    double avgRating;
+    int ratingCount;
+    int popularity;
+};
+
+#endif
---
+++ book-recommendation/include/User.h
@@ -0,0 +1,40 @@
+﻿#ifndef USER_H
+#define USER_H
+
+#include <map>
+#include <set>
+#include <string>
+
+class User {
+public:
+    User();
+    User(int id, const std::string& name);
+
+    int getId() const;
+    const std::string& getName() const;
+    const std::set<std::string>& getPreferredGenres() const;
+    const std::set<std::string>& getPreferredAuthors() const;
+    const std::set<int>& getReadBooks() const;
+    const std::map<int, int>& getRatedBooks() const;
+
+    void setName(const std::string& name);
+    bool addPreferredGenre(const std::string& genre);
+    bool addPreferredAuthor(const std::string& author);
+    bool markBookRead(int bookId);
+    bool addOrUpdateRating(int bookId, int score);
+
+    bool hasRead(int bookId) const;
+    bool hasRated(int bookId) const;
+    int getRatingForBook(int bookId) const;
+    bool isValid() const;
+
+private:
+    int id;
+    std::string name;
+    std::set<std::string> preferredGenres;
+    std::set<std::string> preferredAuthors;
+    std::set<int> readBooks;
+    std::map<int, int> ratedBooks;
+};
+
+#endif
