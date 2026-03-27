898ea9a431ad614fe50138ad08feeed5ad614fda
Ashique-7 <ashiquek056@gmail.com>
2026-03-27 15:38:29 +0600

    Add files via upload

---
+++ book-recommendation/include/RatingRepository.h
@@ -0,0 +1,21 @@
+﻿#ifndef RATINGREPOSITORY_H
+#define RATINGREPOSITORY_H
+#include <string>
+#include <vector>
+#include "Rating.h"
+
+using namespace std;
+
+class RatingRepository{
+public:
+    bool load(const string& path);
+    bool save(const string& path) const;
+    const vector<Rating>& getAll() const;
+    vector<Rating>& getAllMutable();
+    void addOrUpdate(int userId, int bookId, int score);
+
+private:
+    vector<Rating> ratings;
+};
+
+#endif
